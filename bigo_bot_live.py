7108a0a876847f15850ae9128ddab57bba10f0b0
Abdelrhman Tarek <abdelrhman.tarek.elsbagh@gmail.com>
2024-05-08 02:58:07 +0200

    enhance login

--- bigo_bot_live.py
+++ bigo_bot_live.py
@@ -36,7 +36,7 @@ def init_driver():
     options.add_argument(f'--proxy-server=http://{proxy_auth}')
 
     # Additional options for headless mode and performance
-    options.add_argument("--headless")
+    # options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-gpu")
     options.add_argument("--window-size=1920x1080")
