d077d26a7982406660cdaecf94232877b4249034
Abdelrhman Tarek <abdelrhman.tarek.elsbagh@gmail.com>
2024-05-27 07:53:16 +0200

    update 101

--- indv/t5.py
+++ indv/t5.py
@@ -503,7 +503,7 @@ def sigterm_handler(signum, frame):
     sys.exit(0)  # Exit gracefully
 
 def main():
-    sys.setrecursionlimit(10000)
+    sys.setrecursionlimit(100000)
     options = webdriver.ChromeOptions()
     options.add_argument("--headless")
     options.add_argument('--no-sandbox')
