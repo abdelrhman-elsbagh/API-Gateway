c31f3b6080d3079136345ab12cec2cd90ec782c0
Abdelrhman Tarek <abdelrhman.tarek.elsbagh@gmail.com>
2024-08-26 12:18:37 +0200

    cookies

--- indv/t3.py
+++ indv/t3.py
@@ -601,7 +601,7 @@ def main():
     # proxy = "192.168.1.6:30002"
     sys.setrecursionlimit(100000)
     options = webdriver.ChromeOptions()
-    # options.add_argument("--headless")
+    options.add_argument("--headless")
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-gpu')
     options.add_argument('--window-size=1920x1080')
