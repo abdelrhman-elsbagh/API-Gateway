fa644fbe84be0b9d537b59c357a11ec0ef50f36d
Abdelrhman Tarek <abdelrhman.tarek.elsbagh@gmail.com>
2024-09-05 01:58:36 +0200

    accounts

--- indv/restart_script.sh
+++ indv/restart_script.sh
@@ -8,10 +8,10 @@ while true; do
     echo "Starting t3.py at $(date)"
     python3 t3.py &  # Run t3.py in the background
 
-    echo "t3.py started. Waiting for 12 hours before restarting..."
-    sleep 12h  # Wait for 12 hours
+    echo "t3.py started. Waiting for 20 hours before restarting..."
+    sleep 20h  # Wait for 20 hours
 
-    # After 12 hours, delete the log file
+    # After 20 hours, delete the log file
     echo "Deleting log file..."
     rm -f restart_script.log
 done
